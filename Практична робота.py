cf27fe4cb55f7ef97c0da2d3a76573b3f8d78d32
vikkbbu <bujnitskaya2044@gmail.com>
2025-02-26 15:34:25 +0200

    Update Практична робота.py

--- Практична робота.py
+++ Практична робота.py
@@ -3,14 +3,21 @@ from tkinter.messagebox import *
 
 def click(event):
   root["bg"] = "red"
-  new_y = 200
-  new_width = root.winfo_width() + 300
-  new_x = root.winfo_x() - 50
-  root.geometry(f"{new_width}x100+{new_x}+{new_y}")
+  click_y = 200
+  click_width = root.winfo_width() + 300
+  click_x = root.winfo_x() - 50
+  root.geometry(f"{click_width}x100+{click_x}+{click_y}")
   showinfo("", "Ми вивчаємо програмування!")
+
+def key_press(event):
+  root["bg"] = "blue"
+  key_y = root.winfo_y() + 300
+  key_width = root.winfo_width() + 200
+  root.geometry(f"{key_width}x100+{root.winfo_x()}+{key_y}")
 
 root = Tk()
 root["bg"] = "gray"
 root.geometry("200x100+150+100")
 root.title("Практична робота")
 root.bind("<1>", click)
+root.bind("<KeyPress>", key_press)
