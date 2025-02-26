c3ded27716b9db3cb24591cb764a4b653b2aa478
vikkbbu <bujnitskaya2044@gmail.com>
2025-02-26 14:52:53 +0200

    Update Практична робота.py

--- Практична робота.py
+++ Практична робота.py
@@ -1,6 +1,10 @@
 from tkinter import *
 
+def click(event):
+  root["bg"] = "red"
+
 root = Tk()
 root["bg"] = "gray"
 root.geometry("200x100 + 150 + 100")
 root.title("Практична робота")
+root.bind("<1>", click)
